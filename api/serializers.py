8437021a2a88cd20ef03deb93611b770ac6c7333
Liviu Ene <eneliviu@gmail.com>
2025-01-05 19:20:56 +0100

    Fix default permissions and serializer for the project

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,5 @@
-from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
-
+from dj_rest_auth.serializers import UserDetailsSerializer
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
