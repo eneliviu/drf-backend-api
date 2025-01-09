ee61f5d2f2f979b8e7a6c62a0c8d5aa872388a8a
Liviu Ene <eneliviu@gmail.com>
2025-01-09 11:16:33 +0100

    Debug Heroku deployment

--- api/settings.py
+++ api/settings.py
@@ -9,7 +9,7 @@ if os.path.exists('env.py'):
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = False
+DEBUG = True
 
 CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 CLOUDINARY_STORAGE = {
